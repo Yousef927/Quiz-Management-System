4c7af2f5d41c1dd1a8fee3d8982bbe05c3a43b86
yousef hagag <hagagyousef927@gmail.com>
2026-03-26 06:14:14 +0200

    validation annotation added

--- src/main/java/com/hagag/QuizManagementSystem/entities/Question.java
+++ src/main/java/com/hagag/QuizManagementSystem/entities/Question.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
@@ -14,7 +15,9 @@ public class Question {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank
     private String questionTitle;
+
     private String option1;
     private String option2;
     private String option3;
