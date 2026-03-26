55e64e5227515bfbc2f55901adcb5e25bc0e4e51
yousef hagag <hagagyousef927@gmail.com>
2026-03-26 06:03:56 +0200

    added CRUD APIS

--- src/main/java/com/hagag/QuizManagementSystem/controllers/QuestionController.java
+++ src/main/java/com/hagag/QuizManagementSystem/controllers/QuestionController.java
@@ -1,13 +1,12 @@
 package com.hagag.QuizManagementSystem.controllers;
 
 import com.hagag.QuizManagementSystem.DAOS.QuestionRepository;
+import com.hagag.QuizManagementSystem.DTOS.QuestionRequestDTO;
 import com.hagag.QuizManagementSystem.entities.Question;
 import com.hagag.QuizManagementSystem.services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +23,21 @@ public class QuestionController {
     @GetMapping("/getQuestions")
     public ResponseEntity<List<Question>> getQuestions() {
         return questionService.getAllQuestions();
+    }
+
+    @PostMapping("/addQuestion")
+    public ResponseEntity<String> addQuestion(@RequestBody QuestionRequestDTO question) {
+        return questionService.addQuestion(question);
+    }
+
+    @DeleteMapping("/deleteQuestion/{id}")
+    public ResponseEntity<String> deleteQuestion(@PathVariable Integer id) {
+        return questionService.deleteQuestion(id);
+    }
+
+    @PutMapping("/updateQuestion/{id}")
+    public  ResponseEntity<String> updateQuestion(@PathVariable Integer id , @RequestBody QuestionRequestDTO questionRequest) {
+        return questionService.updateQuestion(id , questionRequest);
     }
 
 
