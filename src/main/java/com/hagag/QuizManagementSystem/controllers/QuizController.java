dda0ff1c5124113d19423c025fcc106656c7a751
yousef hagag <hagagyousef927@gmail.com>
2026-03-26 04:08:09 +0200

    Quiz Controller Added

---
+++ src/main/java/com/hagag/QuizManagementSystem/controllers/QuizController.java
@@ -0,0 +1,9 @@
+package com.hagag.QuizManagementSystem.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/quiz")
+public class QuizController {
+}
