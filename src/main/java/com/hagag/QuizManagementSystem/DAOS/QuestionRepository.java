72f295be1ae8f209bf52baf9d5b0152e0f38442c
yousef hagag <hagagyousef927@gmail.com>
2026-03-26 03:28:01 +0200

    added annotation

--- src/main/java/com/hagag/QuizManagementSystem/DAOS/QuestionRepository.java
+++ src/main/java/com/hagag/QuizManagementSystem/DAOS/QuestionRepository.java
@@ -2,7 +2,9 @@ package com.hagag.QuizManagementSystem.DAOS;
 
 import com.hagag.QuizManagementSystem.entities.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question , Integer> {
 
 
--- src/main/java/com/hagag/QuizManagementSystem/DAOS/QuizRepository.java
+++ src/main/java/com/hagag/QuizManagementSystem/DAOS/QuizRepository.java
@@ -2,6 +2,8 @@ package com.hagag.QuizManagementSystem.DAOS;
 
 import com.hagag.QuizManagementSystem.entities.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuizRepository extends JpaRepository<Quiz, Integer> {
 }
