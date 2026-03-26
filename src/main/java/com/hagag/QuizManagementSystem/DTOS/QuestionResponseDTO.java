fca2e1eb3762786abe52de58dc1962e5b0e03bd1
yousef hagag <hagagyousef927@gmail.com>
2026-03-26 06:12:01 +0200

    added Response DTO

--- src/main/java/com/hagag/QuizManagementSystem/DTOS/QuestionResponseDTO.java
+++ src/main/java/com/hagag/QuizManagementSystem/DTOS/QuestionResponseDTO.java
@@ -1,11 +1,12 @@
 package com.hagag.QuizManagementSystem.DTOS;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class QuestionDTO {
+public class QuestionResponseDTO {
 
     private Integer id;
     private String questionTitle;
@@ -13,4 +14,7 @@ public class QuestionDTO {
     private String option2;
     private String option3;
     private String option4;
+
+
+
 }
