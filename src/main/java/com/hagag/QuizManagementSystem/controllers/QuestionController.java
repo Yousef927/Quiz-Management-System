b269b7c5e3c0c08df889f14d285d138fb88de91e
yousef hagag <hagagyousef927@gmail.com>
2026-03-26 04:06:12 +0200

    feat: getQuestions API

--- src/main/java/com/hagag/QuizManagementSystem/controllers/QuestionController.java
+++ src/main/java/com/hagag/QuizManagementSystem/controllers/QuestionController.java
@@ -2,7 +2,9 @@ package com.hagag.QuizManagementSystem.controllers;
 
 import com.hagag.QuizManagementSystem.DAOS.QuestionRepository;
 import com.hagag.QuizManagementSystem.entities.Question;
+import com.hagag.QuizManagementSystem.services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +18,12 @@ public class QuestionController {
     @Autowired
     QuestionRepository questionRepository;
 
+    @Autowired
+    QuestionService questionService;
+
     @GetMapping("/getQuestions")
-    public List<Question> getQuestions() {
-        return questionRepository.findAll();
+    public ResponseEntity<List<Question>> getQuestions() {
+        return questionService.getAllQuestions();
     }
 
 
