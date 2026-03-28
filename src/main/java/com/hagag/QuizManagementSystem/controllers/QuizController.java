69fbaa8246d724ea74ee18c75c07bd6d748579fa
yousef hagag <hagagyousef927@gmail.com>
2026-03-28 05:35:54 +0200

    create API added

--- src/main/java/com/hagag/QuizManagementSystem/controllers/QuizController.java
+++ src/main/java/com/hagag/QuizManagementSystem/controllers/QuizController.java
@@ -1,9 +1,20 @@
 package com.hagag.QuizManagementSystem.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hagag.QuizManagementSystem.services.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/quiz")
 public class QuizController {
+
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("/createQuiz")
+    public ResponseEntity<String> createQuiz(@RequestParam String quizTitle , @RequestParam Integer numberOfQuestions) {
+        return quizService.createQuiz(quizTitle , numberOfQuestions);
+
+    }
 }
