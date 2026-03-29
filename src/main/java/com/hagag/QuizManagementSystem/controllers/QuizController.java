01d73fb6e2a0cd0e85ac4f812eb207b46a95b631
yousef hagag <hagagyousef927@gmail.com>
2026-03-29 22:11:13 +0200

    quizQuestions API added

--- src/main/java/com/hagag/QuizManagementSystem/controllers/QuizController.java
+++ src/main/java/com/hagag/QuizManagementSystem/controllers/QuizController.java
@@ -1,9 +1,12 @@
 package com.hagag.QuizManagementSystem.controllers;
 
+import com.hagag.QuizManagementSystem.DTOS.QuestionResponseDTO;
 import com.hagag.QuizManagementSystem.services.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
@@ -15,6 +18,10 @@ public class QuizController {
     @PostMapping("/createQuiz")
     public ResponseEntity<String> createQuiz(@RequestParam String quizTitle , @RequestParam Integer numberOfQuestions) {
         return quizService.createQuiz(quizTitle , numberOfQuestions);
+    }
 
+    @GetMapping("/getQuizQuestions/{id}")
+    public ResponseEntity<List<QuestionResponseDTO>> getQuizQuestions(@PathVariable Integer id) {
+        return quizService.getQuizQuestions(id);
     }
 }
