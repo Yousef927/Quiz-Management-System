e1f4af5754aec607ce9f7e624e206c2358eac6a8
yousef hagag <hagagyousef927@gmail.com>
2026-04-01 23:34:20 +0200

    global exception handler added

---
+++ src/main/java/com/hagag/QuizManagementSystem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.hagag.QuizManagementSystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(QuizNotFoundException.class)
+    public ResponseEntity<String> handleQuizNotFound(QuizNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
