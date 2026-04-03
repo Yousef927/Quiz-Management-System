dd759b9268c2017e1798598cebfdf49c8b70daf3
yousef hagag <hagagyousef927@gmail.com>
2026-04-03 17:27:55 +0200

    implemented DTO and combined similar exceptions into one handler

--- src/main/java/com/hagag/QuizManagementSystem/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hagag/QuizManagementSystem/exception/GlobalExceptionHandler.java
@@ -1,30 +1,35 @@
 package com.hagag.QuizManagementSystem.exception;
 
+import com.hagag.QuizManagementSystem.DTOS.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(QuizNotFoundException.class)
-    public ResponseEntity<String> handleQuizNotFound(QuizNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
-    }
+    @ExceptionHandler({QuizNotFoundException.class , QuestionNotFoundException.class})
+    public ResponseEntity<ErrorResponse> handleQuizNotFound(Exception ex) {
 
-    @ExceptionHandler(QuestionNotFoundException.class)
-    public ResponseEntity<String> handleQuestionNotFound(QuestionNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage() , HttpStatus.NOT_FOUND);
+        ErrorResponse errorResponse = ErrorResponse.buildErrorResponse(ex.getMessage() , HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(errorResponse , HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(QuizSubmissionException.class)
-    public ResponseEntity<String> handleQuizSubmission(QuizSubmissionException ex) {
-        return new ResponseEntity<>(ex.getMessage() , HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleQuizSubmission(QuizSubmissionException ex) {
+
+        ErrorResponse errorResponse = ErrorResponse.buildErrorResponse(ex.getMessage() , HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorResponse , HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(QuestionAlreadyExistException.class)
-    public ResponseEntity handleQuestionAlreadyExist(QuestionAlreadyExistException ex) {
-        return new ResponseEntity<>(ex.getMessage() , HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleQuestionAlreadyExist(QuestionAlreadyExistException ex) {
+
+        ErrorResponse errorResponse = ErrorResponse.buildErrorResponse(ex.getMessage() , HttpStatus.CONFLICT);
+        return new ResponseEntity<>(errorResponse , HttpStatus.CONFLICT);
     }
 }
